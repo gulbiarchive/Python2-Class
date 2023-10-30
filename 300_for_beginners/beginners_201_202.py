a75c1422cc81a590bdda79fac5a41fa897b78dac
gulbi <123242543+gulbiarchive@users.noreply.github.com>
2023-10-30 23:35:32 +0900

    Add files via upload

---
+++ 300_for_beginners/beginners_201_202.py
@@ -0,0 +1,8 @@
+# 정의
+def print_coin():
+    print("비트코인")
+    
+# 호출하고 출력
+print_coin() # 비트코인
+    
+    
---
+++ 300_for_beginners/beginners_203.py
@@ -0,0 +1,10 @@
+def print_coint():
+    print("비트코인")
+
+# 반복문을 통해서 범위 지정한 후
+# 100번 호출 
+for i in range(100):
+    print_coint()
+    
+    
+    
