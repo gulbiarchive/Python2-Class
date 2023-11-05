a6289a3cefa649b97cff3ddd596cd0386c6337de
gulbi <123242543+gulbiarchive@users.noreply.github.com>
2023-11-05 22:11:26 +0900

    Add files via upload

---
+++ 100_for_SW_competition/competition_92A.py
@@ -0,0 +1,9 @@
+def new_add(n):
+    sum = 0
+    for i in range(n):
+        sum += i
+    print('1부터', n, '까지 합은', sum, '입니다.')
+
+new_add(10)
+new_add(100)
+
---
+++ 100_for_SW_competition/competition_92B.py
@@ -0,0 +1,14 @@
+a = [1, 2, 3, 4, 5]
+
+def add(a):
+    sum = 0
+    for i in a:
+        sum += i
+    print(sum)
+    
+add(a)
+
+
+
+    
+    
