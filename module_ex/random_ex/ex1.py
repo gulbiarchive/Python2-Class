6a799df60d8cc4d1e89f0e9e79d56c05883d7d38
gulbi <123242543+gulbiarchive@users.noreply.github.com>
2023-11-06 00:14:02 +0900

    Add files via upload

---
+++ module_ex/random_ex/ex1.py
@@ -0,0 +1,8 @@
+import random
+
+print(random.random())
+print(random.randint(1, 2))
+print(random.randrange(1, 3))
+print(random.uniform(1, 3))
+print(random.choice(["Python", "Java", "C", "C++"]))
+
